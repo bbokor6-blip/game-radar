6c5617f1419f90c4fdcd2fcd12e215b807c4c08f
bbokor6-blip <bbokor6@gmail.com>
2026-09-20 23:59:07 -0400

    Show full game dates throughout GameRadar

--- app/scores/page.js
+++ app/scores/page.js
@@ -31,7 +31,7 @@ function rangeLabel(range){
 }
 
 function gameTime(game){
-  return new Date(game.date).toLocaleString([],{weekday:"short",hour:"numeric",minute:"2-digit"});
+  return new Date(game.date).toLocaleString([],{weekday:"short",month:"short",day:"numeric",hour:"numeric",minute:"2-digit"});
 }
 
 function matchupLabel(game){
